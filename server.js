d9c7a9356921afc847868351641cff25bbb0df96
Esteban Herrera <esherrer@microsoft.com>
2019-01-10 16:58:33 -0800

    working, added winston.js

--- server.js
+++ server.js
@@ -2,6 +2,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
 const app = express()
+const {transports, createLogger, format} = require('winston');
+
+
+const logger = createLogger({
+  level: 'info',
+  format: format.combine(format.timestamp(), format.json()),
+  defaultMeta: {service: 'weather-app'},
+  transports: [
+    //
+    // - Write to all logs with level `info` and below to `combined.log` 
+    // - Write all logs error (and below) to `error.log`.
+    //
+    new transports.File({ filename: 'error.log', level: 'error' }),
+    new transports.File({ filename: 'combined.log' })
+  ]
+});
 
 const apiKey = '*****************';
 
@@ -20,10 +36,12 @@ app.post('/', function (req, res) {
   request(url, function (err, response, body) {
     if(err){
       res.render('index', {weather: null, error: 'Error, please try again'});
+      logger.log({ level: 'error', message: 'failed to find city weather', city: city});
     } else {
       let weather = JSON.parse(body)
       if(weather.main == undefined){
         res.render('index', {weather: null, error: 'Error, please try again'});
+        logger.log({ level: 'error', message: 'failed to find city weather', city: city});
       } else {
         let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
         res.render('index', {weather: weatherText, error: null});
